2b4278b15371b0c35c9d62d785d044df1e105341
Ajoy Sinha <ajoy.sinha@live.com>
2018-10-29 21:02:20 +0530

    Es6 coding style with scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+// How to run and test it
+// Run with node repl ( Read - Event - Point - Loop )
+
+const name = "Ajoy Kr Sinha"
+
+// Method declare using ES6 ECMAScript6
+const showName = () => {
+  console.log(" My name is "+name);
+}
+
+// Call method
+showName();
